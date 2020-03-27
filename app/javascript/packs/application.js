f47c14cda02c30752684b45686a98f2d46c33652
小島怜 <r.kojima@Rei.local>
2020-03-27 20:56:33 +0900

    ja

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("jquery")
+import "jquery"
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("channels")
-require("bootstrap-sprockets")
+import "channels"
+import "bootstrap-sprockets"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
