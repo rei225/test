e08b7abeeeceab38eb6f46e256b03d961f75651d
小島怜 <r.kojima@Rei.local>
2020-03-27 19:21:58 +0900

    initial commit

---
+++ app/controllers/memos_controller.rb
@@ -0,0 +1,14 @@
+class MemosController < ApplicationController
+    def index
+        #トップページが呼ばれた時に動作するアクション
+        @memos = Memo.all
+    end
+    def new 
+        #新規作成ページが呼ばれた気に動作するアクション
+    end
+    def create
+        #新しいメモがフォームからPostされた時に動作するアクション
+        Memo.create(title: params["memos"]["title"],body:params["memos"]["body"])
+        redirect_to "/"
+    end
+end
